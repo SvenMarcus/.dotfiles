a1f154989c23955bbf798cd9a6c281219619b3d6
Sven Marcus <s.marcus@outlook.de>
2025-09-17 16:14:02 +0200

    experiments with mypy detection

--- nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,5 +1,17 @@
 local pdm_venv_path = vim.fn.expand("~") .. "/.local/share/pdm/venvs"
 
+local function on_venv_activate()
+  -- local venv = require("venv-selector").venv()
+  -- local mypy = venv .. "/bin/mypy"
+  -- if vim.fn.filereadable(mypy) == 1 then
+  --   Snacks.notifier.notify("mypy was found in the selected venv", "info")
+  --   local null_ls = require("null-ls")
+  --   null_ls.builtins.diagnostics.mypy.with({
+  --     only_local = mypy,
+  --   })
+  -- end
+end
+
 return {
   "linux-cultist/venv-selector.nvim",
   opts = {
@@ -11,6 +23,7 @@ return {
       },
       options = {
         picker = "snacks",
+        on_venv_activate_callback = on_venv_activate,
       },
     },
   },
