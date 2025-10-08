b2d8b5a26b0abe4a3d7344df4d5c4a36a3502d4c
Sven Marcus <s.marcus@outlook.de>
2025-10-08 19:15:12 +0200

    remap ö and ä to [ and ]

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 
 map("n", "Ö", "{", { desc = "Previous Paragraph" })
 map("n", "Ä", "}", { desc = "Next Paragraph" })
 
-map("n", "ö", "[")
-map("n", "ö", "]")
+map({ "n", "v" }, "ö", "[", { remap = true, desc = "Previous prefix" })
+map({ "n", "v" }, "ä", "]", { remap = true, desc = "Next prefix" })
 
 map("n", "<C-->", function()
   Snacks.terminal(nil, { cwd = LazyVim.root() })
