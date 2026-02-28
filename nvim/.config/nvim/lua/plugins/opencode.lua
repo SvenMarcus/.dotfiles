3be8a91efed450eb53cf3014aec8588942f324b1
Sven Marcus <s.marcus@outlook.de>
2026-02-28 03:35:44 +0100

    update opencode.nvim config with newer snacks integration

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -1,10 +1,54 @@
 return {
   "NickvanDyke/opencode.nvim",
-  dependencies = { "folke/snacks.nvim", opts = { input = { enabled = true } } },
+  dependencies = {
+    {
+      ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+      "folke/snacks.nvim",
+      optional = true,
+      opts = {
+        input = {}, -- Enhances `ask()`
+        picker = { -- Enhances `select()`
+          actions = {
+            opencode_send = function(...)
+              return require("opencode").snacks_picker_send(...)
+            end,
+          },
+          win = {
+            input = {
+              keys = {
+                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   config = function()
+    local opencode_cmd = "opencode --port --agent OpenAgent"
+    ---@type snacks.terminal.Opts
+    local snacks_terminal_opts = {
+      win = {
+        position = "right",
+        enter = false,
+        on_win = function(win)
+          -- Set up keymaps and cleanup for an arbitrary terminal
+          require("opencode.terminal").setup(win.win)
+        end,
+      },
+    }
+    ---@type opencode.Opts
     vim.g.opencode_opts = {
-      provider = {
-        cmd = "opencode --port --agent OpenAgent",
+      server = {
+        start = function()
+          require("snacks.terminal").open(opencode_cmd, snacks_terminal_opts)
+        end,
+        stop = function()
+          require("snacks.terminal").get(opencode_cmd, snacks_terminal_opts):close()
+        end,
+        toggle = function()
+          require("snacks.terminal").toggle(opencode_cmd, snacks_terminal_opts)
+        end,
       },
     }
   end,
